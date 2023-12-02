0db163b6a18921a9d44eee23ec555ab5e6eb063b
it's <129371128+underbari7zunderbar@users.noreply.github.com>
2023-12-02 09:37:13 +0900

    Delete src/main/kotlin/site/revanilla/avatar/AvatarKeychain.kt

--- src/main/kotlin/site/revanilla/avatar/AvatarKeychain.kt
+++
@@ -1,8 +0,0 @@
-/*package site.revanilla.avatar
-
-import io.github.monun.tap.data.PersistentDataKeychain
-
-object AvatarKeychain : PersistentDataKeychain() {
-  val UUID_MOST = complex<Long>("UUID_MOST")
-  val UUID_LEAST = complex<Long>("UUID_LEAST")
-}*/
